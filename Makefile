1f703d42b4ab2bace87681b9f28cd3051085ecdf
Eduardo Santos <edsantos@redhat.com>
2025-06-02 19:59:53 -0300

    refactor: handle SIGTERM

--- Makefile
+++ Makefile
@@ -3,5 +3,10 @@ CFLAGS=-static -Wall
 
 all:
 	$(CC) $(CFLAGS) -o comp2 comp2.c
+check:
+	$(CC) $(CFLAGS) -o comp2 comp2.c
+	-./comp2
+	-./comp2 foo
+	rm comp2
 clean:
 	rm -f comp2
--- comp2.c
+++ comp2.c
@@ -1,12 +1,37 @@
 #include <fcntl.h>
+#include <signal.h>
 #include <stdio.h>
+#include <string.h>
 #include <unistd.h>
 
-int
-main()
+#define RST "\e[0m"
+
+volatile sig_atomic_t done = 0;
+
+void term(int signum)
 {
+	done = 1;
+}
+
+int
+main(int argc, char *argv[])
+{
+	struct sigaction act;
+	memset(&act, 0, sizeof(struct sigaction));
+	act.sa_handler = term;
+	sigaction(SIGTERM, &act, NULL);
+
+	if (argc > 1) {
+		fprintf(stderr,
+		        "\e[1;91merr: comp1 takes no arguments"RST"\n");
+		return 1;
+	}
 	while (1) {
-		int fd = open("./file", O_RDONLY);
+		fprintf(stderr,
+		        "\e[1;95mwasting one file descriptor every 5s... :("RST
+		        "\n");
+		open("./foo", O_RDONLY);
+		/* int fd = open("./foo", O_RDONLY); */
 		sleep(5);
 		/* close(fd); */
 	}
